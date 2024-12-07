e09ddbb47310a99f30c3aa1e9ea3c10f907a9343
shakya1226 <160268366+shakya1226@users.noreply.github.com>
2024-12-07 18:36:53 +0530

    second

--- src/Main.java
+++ src/Main.java
@@ -102,5 +102,4 @@ public class Main {
 
         running = false;
     }
-
 }
--- src/TicketPool.java
+++ src/TicketPool.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 public class TicketPool {
     private final int maxCapacity;
     private final Queue<Ticket> tickets = new LinkedList<>();
-    private int totalTickets;
+    private int totalTickets; // Keeps track of total ticket count to generate unique IDs
 
     public TicketPool(int maxCapacity, int initialTotalTickets) {
         this.maxCapacity = maxCapacity;
@@ -16,8 +16,9 @@ public class TicketPool {
         }
     }
 
-    // Method to add tickets to the pool
+    // Method to add tickets to the pool (called by vendors)
     public synchronized void addTickets(int vendorId, int ticketsToAdd) {
+        // Check if there is enough capacity to add the tickets
         if (tickets.size() + ticketsToAdd <= maxCapacity) {
             for (int i = 0; i < ticketsToAdd; i++) {
                 tickets.add(new Ticket(totalTickets + 1)); // Add a new ticket with a unique ID
@@ -29,11 +30,12 @@ public class TicketPool {
         }
     }
 
-    // Method to retrieve tickets from the pool (Customer buys tickets)
+    // Method to retrieve tickets from the pool (called by customers)
     public synchronized void retrieveTickets(int customerId, int ticketsToRetrieve) {
         int ticketsPurchased = 0;
         StringBuilder purchasedTicketIds = new StringBuilder();
 
+        // Retrieve the tickets
         while (ticketsToRetrieve > 0 && !tickets.isEmpty()) {
             Ticket ticket = tickets.poll(); // Remove ticket from the pool
             purchasedTicketIds.append(ticket.getId()).append(" "); // Collect Ticket IDs
@@ -41,13 +43,17 @@ public class TicketPool {
             ticketsPurchased++;
         }
 
+        // Log the result of the purchase
         if (ticketsPurchased > 0) {
-            System.out.println("Customer-" + customerId + " purchased " + ticketsPurchased + " tickets: " + purchasedTicketIds +
-                    ". Remaining tickets: " + tickets.size());
+            System.out.println("Customer-" + customerId + " purchased " + ticketsPurchased + " tickets: " +
+                    purchasedTicketIds.toString() + ". Remaining tickets: " + tickets.size());
         } else {
             System.out.println("Customer-" + customerId + " tried purchasing tickets, but none were available.");
         }
     }
 
-
+    // Get the current number of tickets in the pool
+    public synchronized int getTicketCount() {
+        return tickets.size();
+    }
 }
